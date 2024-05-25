91fa07e01825f8183dae41dab1810a086d0edb68
UmairAhmedImran <umairahmedimran@gmail.com>
2024-05-25 20:03:04 +0500

    Client talking to our django project

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,10 +1,11 @@
 import requests
 
-endpoint = "http://httpbin.org/anything"
+# endpoint = "http://httpbin.org/anything"
+endpoint = "http://localhost:8000/"
 
 get_response = requests.get(endpoint, json={"query": "Hello World"})
 
 print(get_response.text)
 
-print(get_response.json())
+# print(get_response.json())
 print(get_response.status_code)
