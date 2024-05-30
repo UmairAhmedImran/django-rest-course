aaa02816b113a74456377826124462b0ccbb08c6
UmairAhmedImran <umairahmedimran@gmail.com>
2024-05-30 19:15:56 +0500

    Using request to get data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,18 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Json Response"})
+    body = request.body
+    print(request.GET)
+    print(request.POST)
+
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass 
+    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content)type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,9 +3,9 @@ import requests
 # endpoint = "http://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"query": "Hello World"})
+get_response = requests.get(endpoint, params={'abc': 123},json={"query": "Hello World"})
 
-print(get_response.text)
+#print(get_response.text)
 
-print(get_response.json()['message'])
-print(get_response.status_code)
+print(get_response.json())
+#print(get_response.status_code)
