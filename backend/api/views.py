712762e4b1cdb5a701e7dc6251080f82d7f7a143
UmairAhmedImran <umairahmedimran@gmail.com>
2024-06-05 10:55:12 +0500

    Using model_to_dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 import json
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 from products.models import Products
 
 
@@ -24,8 +25,12 @@ def api_home(request, *args, **kwargs):
 
     if model_data:
 
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
+        # instead of all above we can write
+        data = model_to_dict(model_data, fields=[
+                             'id', 'title', 'price', 'content'])
+
     return JsonResponse(data)
